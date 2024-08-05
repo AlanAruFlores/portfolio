5c113d916dc4fdd5bc0bfa503b95a51d32aeeec9
AlanAruFlores <alanaruquipa242@gmail.com>
2024-08-05 14:17:10 -0300

    algunos arreglos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,7 +15,8 @@ export default {
 			},
 			backgroundImage:{
 				"wave": "url('../img/wave.svg')",
-				"wave-reverse": "url('../img/wave-reverse.svg')"
+				"wave-reverse": "url('../img/wave-reverse.svg')",
+				"pixel": "url('../img/pixel.svg')"
 			},
 			fontFamily:{
 				"merriweather":'"Merriweather Sans", sans-serif',
@@ -24,8 +25,12 @@ export default {
 			boxShadow:{
 				"custom": "0px 0px 4px 1px #FFFFFF",
 				"custom-blue": "0px 0px 15px 10px #071952",
-				"custom-blue-hover": "0px 0px 15px 20px #071952"
-
+				"custom-blue-hover": "0px 0px 15px 20px #071952",
+				"custom-blue-two": "0px 0px 10px 2px #071952",
+				"custom-blue-min": "0px 0px 4px 4px #071952",
+			},
+			dropShadow:{
+				'blue-custom': '0 0 20px #2267F2',
 			}
 		},
 	},
